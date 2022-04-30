9f352bf1aa79a9546e3fdb7af30054ec1127a960
paulinegc <paulinegc@protonmail.com>
2022-04-30 20:08:30 +0200

    button install

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,36 @@ import Head from 'next/head';
 import { useEffect } from 'react';
 
 export default function Home() {
+    let promptEvent = null;
+
+    const listenToUserAction = () => {
+        const installBtn = document.querySelector('#button');
+        installBtn.addEventListener('click', presentAddToHome);
+    };
+
+    const presentAddToHome = () => {
+        promptEvent.prompt();  // Wait for the user to respond to the prompt
+        promptEvent.userChoice
+            .then(choice => {
+                if (choice.outcome === 'accepted') {
+                    console.log('User accepted');
+                } else {
+                    console.log('User dismissed');
+                }
+            })
+    }
 
     useEffect(() => {
         if ('serviceWorker' in navigator) {
 
             window.addEventListener('fetch', () => console.log('fetch'));
 
-            window.addEventListener('beforeinstallprompt', function () {
+            window.addEventListener('beforeinstallprompt', function (e) {
                 console.log('==== beforeinstallprompt ==== ');
+
+                e.preventDefault();
+                promptEvent = e;
+                listenToUserAction();
             });
 
             window.addEventListener('load', function () {
@@ -36,14 +58,15 @@ export default function Home() {
     return <div>
         <Head>
             <title>SaS Fidelity</title>
-            <meta charSet="utf-8" />
-            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            <meta charSet="utf-8"/>
+            <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
             <link rel="manifest" href="manifest.json"/>
             <link rel="apple-touch-icon" href="img/192x192.png"/>
             <meta name="theme-color" content="#23e3b4"/>
         </Head>
 
         <h1>Hello world</h1>
+        <button id="button">Installer</button>
 
     </div>;
 }

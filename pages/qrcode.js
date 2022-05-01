cc949beab5dff517b16993ca5133f4c47f1beab9
paulinegc <paulinegc@protonmail.com>
2022-05-01 18:22:39 +0200

    wip

---
+++ pages/qrcode.js
@@ -0,0 +1,11 @@
+import QRCode from 'react-qr-code';
+
+export default function Code() {
+
+    const email = localStorage.getItem('fidelity-email')
+
+    return <section>
+        {email}
+        <QRCode value={email}/>
+    </section>;
+}

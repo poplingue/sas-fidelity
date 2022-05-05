bb99b4e6bc193057e970196dd0b7ed6e39fc463f
paulinegc <paulinegc@protonmail.com>
2022-05-05 17:49:57 +0200

    without workbox

--- next.config.js
+++ next.config.js
@@ -1,29 +1,11 @@
 const withOffline = require("next-offline");
-const withPWA = require("next-pwa");
-const runtimeCaching = require("next-pwa/cache");
 const withPlugins = require("next-compose-plugins");
 
-module.exports = withPlugins(
-  [
-    withOffline(),
-    withPWA({
-      disable: process.env.NODE_ENV === "development",
-      reactStrictMode: true,
-      pwa: {
-        dest: "public",
-        register: true,
-        skipWaiting: true,
-        runtimeCaching,
-        buildExcludes: [/middleware-manifest.json$/],
-      },
-    }),
-  ],
-  {
-    serverRuntimeConfig: {
-      emailPassword: process.env.EMAIL_PASSWORD,
-    },
-    publicRuntimeConfig: {
-      baseUrl: process.env.BASE_URL,
-    },
-  }
-);
+module.exports = withPlugins([withOffline()], {
+  serverRuntimeConfig: {
+    emailPassword: process.env.EMAIL_PASSWORD,
+  },
+  publicRuntimeConfig: {
+    baseUrl: process.env.BASE_URL,
+  },
+});
